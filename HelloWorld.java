02a314e6931ee5d3f2dc02d8752b04e468ffbd11
7207550407 <83601639+7207550407@users.noreply.github.com>
2022-07-27 12:21:34 +0530

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,11 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		String s="Hello World"; 
+		System.out.println("Another way to Print :" + s);
+
+	}
+
+}
